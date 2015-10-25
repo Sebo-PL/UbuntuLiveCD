833a39c58be232574fc74e1a633c593bcd05abce
Sebastian Sawicki (PGP:0x5CB361E552FB0D10) <Sebo.PL+UbuntuLiveCD@gMail.com>
2015-10-25 15:09:48 +0100

    Initialize the prerequisites check script
    This is intended to set-up host machine so that it would be
    capable to build a custom Ubuntu Live CD *.iso immage.
    
    Change-Id: I4b03f426349c205c8ed49e131fb2dba1096db090

---
+++ prerequisites.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#
+# This is './prerequisites.sh' file.
+# This file is part of Ubuntu Live CD Customization.
+#
+# Copyright 2015 Sebastian Sawicki
+# E: sebastian.sawicki@seeyour.net
+# PGP: 0x5CB361E552FB0D10
+# M: +48.728454130
+#
+
